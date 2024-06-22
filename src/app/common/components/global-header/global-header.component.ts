2796583b9bc65b52a452bc7d7af6880a3f53bd75
john <urfriendlazy@gmail.com>
2024-06-21 23:11:52 -0400

    created global header common component.
    added bootstrap, angular materials

---
+++ src/app/common/components/global-header/global-header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-global-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule
+  ],
+  templateUrl: './global-header.component.html',
+  styleUrl: './global-header.component.scss'
+})
+export class GlobalHeaderComponent {
+
+}
