f841b1bccb870a5412db52958f5286bcd850815d
john <urfriendlazy@gmail.com>
2024-06-22 18:14:57 -0400

    global header layout
    added css for page containers
    
    create homepage component
    
    added routing for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home",
+    component: HomepageComponent
+  },
+  {
+    path: "**",
+    redirectTo: 'home',
+  }
+];
