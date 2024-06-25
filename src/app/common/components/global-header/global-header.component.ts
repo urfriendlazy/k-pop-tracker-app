956380fa7fb1600a57da91dce84c0c89f6d88140
JT <jt.tran911@gmail.com>
2024-06-25 01:58:21 -0400

    Added image of newjeans, added description for newjeans, added a tab with no function.

--- src/app/common/components/global-header/global-header.component.ts
+++ src/app/common/components/global-header/global-header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
+
 @Component({
   selector: 'app-global-header',
   standalone: true,
@@ -13,3 +14,4 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 export class GlobalHeaderComponent {
 
 }
+
