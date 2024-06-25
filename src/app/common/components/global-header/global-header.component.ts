5cf3fd6f1cd6f5b3dfa71c2fafa28604f5b4571a
JT <jt.tran911@gmail.com>
2024-06-25 12:22:19 -0400

    Redo, moved edits to homepage instead of global-header, fixed tab function.

--- src/app/common/components/global-header/global-header.component.ts
+++ src/app/common/components/global-header/global-header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @Component({
   selector: 'app-global-header',
   standalone: true,
   imports: [
-    MatToolbarModule
+    MatToolbarModule, MatTabsModule
   ],
   templateUrl: './global-header.component.html',
   styleUrl: './global-header.component.scss'
